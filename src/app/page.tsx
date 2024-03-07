abd3d4f30659f99d90f9dc384368ce717c959fd3
Alain369 <alain.lilic@outlook.com>
2024-03-07 13:01:22 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import BtnOutline from "@/components/exports/BtnOutline";
+import { Menu } from "@/components/exports/Menubar";
+import Nav from "@/components/exports/Nav";
+import Button  from "@/components/exports/Button";
+import ModeToggle from "@/components/ui/button-dark";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>  
+    <div className="flex justify-between p-8 text-center  bg-gray-400 dark:bg-gray-800 rounded-3xl ">
+      <div className="">    
+      <Menu  />
+      </div>    
+      <h1 className="text-center font-bold text-white text-3xl">GFN AP2 Vorbereitung</h1>  
+
+      <div className="flex text-end ">
+     <Button />
+      <ModeToggle />
+      </div>
+    </div>
+    </>
+    );
+}
---
+++ src/components/exports/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div><button className="shadow-[0_4px_14px_0_rgb(0,0,0,10%)] hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] px-8 py-2 bg-[#fff] text-[#696969] rounded-md  font-bold transition duration-200 ease-linear dark:bg-black dark:text-white dark:border-white">
+    Login
+  </button></div>
+  )
+}
+
+export default Button
